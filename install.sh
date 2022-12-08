049c300752963dc9b7455baf98b73d7cd0dca523
gromp43 <grompsjunk@gmail.com>
2022-12-08 01:22:54 -0500

    Delete install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir /storage/kintaro
-cp -i pcb.py /storage/kintaro/pcb.py
-chmod +x /storage/kintaro/pcb.py
-cp -i kintaro.service /storage/.config/system.d/kintaro.service
-systemctl enable kintaro.service
