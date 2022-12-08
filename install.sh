a1102561ef43be3c552151f06fdd89520875d11d
gromp43 <grompsjunk@gmail.com>
2022-12-08 01:10:29 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,26 +1,7 @@
 #!/bin/bash
 
-function install_script {
-    mkdir /opt/kintaro
-    cp -i pcb.py /opt/kintaro/pcb.py
-    chmod +x /opt/kintaro/pcb.py
-    useradd -r -s /bin/false kintaro
-    chown -R kintaro:kintaro /opt/kintaro
-    cp -i kintaro.service /etc/systemd/system/kintaro.service
-    systemctl daemon-reload
-    systemctl enable kintaro
-}
-
-if [[ $EUID -ne 0 ]]; then
-  echo "You must be a root user. Try to run the script with sudo" 2>&1
-  exit 1
-fi
-arch=`uname -m`
-if [ "$arch" == "armv6l" ] || [ "$arch" == "armv7l" ] 
-then
-   echo "Raspberry detected"
-   install_script
-else 
-   echo "This script will only run on RaspberryPi"
-   exit 1
-fi
+mkdir /storage/kintaro
+cp -i pcb.py /storage/kintaro/pcb.py
+chmod +x /storage/kintaro/pcb.py
+cp -i kintaro.service /storage/.config/system.d/kintaro.service
+systemctl enable kintaro.service
